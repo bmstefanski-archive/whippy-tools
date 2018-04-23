90d0f800fd9a067085de6f33a23da9728f01f626
Bartłomiej Stefański <bstefanskicom@gmail.com>
2018-04-23 20:12:29 +0200

    Implement test for unique id utils

---
+++ tools/src/test/java/pl.bmstefanski.tools/util/UUIDUtilsTest.java
@@ -0,0 +1,38 @@
+package pl.bmstefanski.tools.util;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+public class UUIDUtilsTest {
+
+    @Test
+    public void shouldHas16BytesFromUniqueId() {
+        UUID uniqueId = UUID.randomUUID();
+        byte[] bytes = UUIDUtils.getBytesFromUUID(uniqueId);
+
+        assertEquals(16, bytes.length);
+    }
+
+    @Test
+    public void shouldReconstructSameUniqueIdFromBytes() {
+        UUID uniqueId = UUID.randomUUID();
+        byte[] bytes = UUIDUtils.getBytesFromUUID(uniqueId);
+        UUID newUniqueId = UUIDUtils.getUUIDFromBytes(bytes);
+
+        assertEquals(uniqueId, newUniqueId);
+    }
+
+    @Test
+    public void shouldGenerateSameUniqueIdFromBytesWhenItIsFromString() {
+        UUID uniqueId = UUID.fromString("02cea57b-a0fb-4391-985e-89e76d642240");
+
+        byte[] bytes = UUIDUtils.getBytesFromUUID(uniqueId);
+        UUID newUniqueId = UUIDUtils.getUUIDFromBytes(bytes);
+
+        assertEquals(uniqueId, newUniqueId);
+    }
+
+}
