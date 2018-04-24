368d9156166e26c43ebb665e6222cb8cf268c4f8
Bartłomiej Stefański <bstefanskicom@gmail.com>
2018-04-24 15:47:49 +0200

    Tests for gamemode utilities

---
+++ tools/src/test/java/pl.bmstefanski.tools/util/GameModeUtilsTest.java
@@ -0,0 +1,41 @@
+package pl.bmstefanski.tools.util;
+
+import org.bukkit.GameMode;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameModeUtilsTest {
+
+    @Test
+    public void shouldReturnCorrectEnumFromNumberValue() {
+        GameMode gameMode = GamemodeUtils.parseGameMode("1");
+        GameMode newGameMode = GameMode.CREATIVE;
+
+        assertEquals(gameMode, newGameMode);
+    }
+
+    @Test
+    public void shouldReturnProperEnmmFromStringValue() {
+        GameMode gameMode = GamemodeUtils.parseGameMode("survival");
+        GameMode newGameMode = GameMode.SURVIVAL;
+
+        assertEquals(gameMode, newGameMode);
+    }
+
+    @Test
+    public void failWhenTheExpectedValueIsNotTheSame() {
+        GameMode gameMode = GamemodeUtils.parseGameMode("adventure");
+        GameMode newGameMode = GameMode.SPECTATOR;
+
+        assertNotEquals(gameMode, newGameMode);
+    }
+
+    @Test
+    public void failWhenTheValueDoesNotExist() {
+        GameMode gameMode = GamemodeUtils.parseGameMode("extraMode");
+
+        assertNull(gameMode);
+    }
+
+}
