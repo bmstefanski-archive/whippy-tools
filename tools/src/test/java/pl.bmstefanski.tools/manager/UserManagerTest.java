f8ae70597406f28eb7028aeaf25062c4bd4a2da4
Bartłomiej Stefański <bstefanskicom@gmail.com>
2018-04-25 16:50:14 +0200

    Improve user manager unit tests

--- tools/src/test/java/pl.bmstefanski.tools/manager/UserManagerTest.java
+++ tools/src/test/java/pl.bmstefanski.tools/manager/UserManagerTest.java
@@ -1,19 +1,49 @@
 package pl.bmstefanski.tools.manager;
 
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.UnimplementedOperationException;
+import org.bukkit.Bukkit;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import pl.bmstefanski.tools.Tools;
+import pl.bmstefanski.tools.basic.User;
 import pl.bmstefanski.tools.impl.manager.UserManagerImpl;
 
 import java.util.UUID;
+
+import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserManagerTest {
 
     @Mock
     private Tools plugin;
+
+    @BeforeClass
+    public static void initialize() {
+        ServerMock serverMock = new ServerMock();
+        Bukkit.setServer(serverMock);
+    }
+
+    @Test(expected = UnimplementedOperationException.class) // waiting for implementation https://github.com/seeseemelk/MockBukkit/blob/master/src/main/java/be/seeseemelk/mockbukkit/ServerMock.java#L876
+    public void userNameAndUniqueIdCannotBeNullWhenCalledByUniqueId() {
+        UUID uniqueId = UUID.randomUUID();
+        User user = new UserManagerImpl(this.plugin).getUser(uniqueId);
+
+        assertNotNull(user.getUUID());
+        assertNotNull(user.getName());
+    }
+
+    @Test(expected = UnimplementedOperationException.class) // waiting for implementation https://github.com/seeseemelk/MockBukkit/blob/master/src/main/java/be/seeseemelk/mockbukkit/ServerMock.java#L876
+    public void userNameAndUniqueIdCannotBeNullWhenCalledByNickname() {
+        User user = new UserManagerImpl(this.plugin).getUser("examplePlayer");
+
+        assertNotNull(user.getUUID());
+        assertNotNull(user.getName());
+    }
 
     @Test(expected = NullPointerException.class)
     public void addUserMethodShouldThrowNullPointerExceptionWhenUserIsNull() {
