07a0fcd966bcae7ae6564d67a82af153a4d46edc
bmstefanski <bstefanskicom@gmail.com>
2018-05-26 00:10:15 +0200

    Improve message util tests

--- tools/src/main/java/pl/bmstefanski/tools/impl/util/MessageUtil.java
+++ tools/src/main/java/pl/bmstefanski/tools/impl/util/MessageUtil.java
@@ -2,10 +2,20 @@ package pl.bmstefanski.tools.impl.util;
 
 import org.bukkit.ChatColor;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public final class MessageUtil {
 
   public static String colored(String message) {
     return message != null ? ChatColor.translateAlternateColorCodes('&', message) : null;
+  }
+
+  public static List<String> colored(List<String> messages) {
+    return messages.stream()
+      .map(MessageUtil::colored)
+      .collect(Collectors.toList());
   }
 
   private MessageUtil() {
--- tools/src/test/java/pl.bmstefanski.tools/impl/util/MessageUtilTest.java
+++ tools/src/test/java/pl.bmstefanski.tools/impl/util/MessageUtilTest.java
@@ -2,16 +2,27 @@ package pl.bmstefanski.tools.impl.util;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MessageUtilTest {
 
   @Test
-  public void shouldReturnReplacedChar() {
-    String originalMessage = "§";
-    String changedMessage = MessageUtil.colored(originalMessage);
+  public void replaceSingleStringTest() {
+    String originalMessage = "§d";
+    String changedMessage = MessageUtil.colored("&d");
 
     assertEquals(originalMessage, changedMessage);
+  }
+
+  @Test
+  public void replaceStringListTest() {
+    List<String> originalList = Arrays.asList("§1", "§2", "§3");
+    List<String> replacedList = MessageUtil.colored(Arrays.asList("&1", "&2", "&3"));
+
+    assertEquals(originalList, replacedList);
   }
 
 }
